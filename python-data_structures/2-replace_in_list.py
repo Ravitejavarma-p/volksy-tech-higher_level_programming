d08e0095fe5aaa3006dcc4119e9bb49cd7b568f4
Ravitejavarma-p <ravitejav@brightcomgroup.com>
2023-01-05 01:58:19 -0800

    2-Task

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -4,6 +4,7 @@ def replace_in_list(my_list, idx, element):
         return my_list
     elif idx >= len(my_list):
         return my_list
-    elif  my_list[idx] = element:
+    elif:
+        my_list[idx] = element:
         return my_list
 
