be3030c673ee89412558a0d58447831db61296a9
Ravitejavarma-p <ravitejav@brightcomgroup.com>
2023-01-05 03:54:08 -0800

    5-Task

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     li = []
     for i in my_string:
-        if i ! = 'c' and i ! = 'C':
+        if i != 'c' and i != 'C':
             li.append(i)
     return "".join(li)    
