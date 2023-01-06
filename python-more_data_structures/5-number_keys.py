e7f31d06c6b1c7a034fad2930e6dc9378849862e
Ravitejavarma-p <ravitejav@brightcomgroup.com>
2023-01-06 03:49:19 -0800

    5

--- python-more_data_structures/5-number_keys.py
+++ python-more_data_structures/5-number_keys.py
@@ -2,7 +2,7 @@
 def number_keys(a_dictionary):
     # if c is not in c
     lst = []
-    for x in my_string:
+    for x in a_dictionary:
         if x != 'c' and x != 'c':
             lst.append(x)
     return "".join(lst)
