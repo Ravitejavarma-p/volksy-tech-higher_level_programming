3301e198653cec07ee28c27b70488ae3f52edaee
Ravitejavarma-p <ravitejav@brightcomgroup.com>
2023-01-09 02:26:04 -0800

    2-Task

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     e = 0
-    for i in range(0, x):
+    for i in range(x):
         try:
             print("{:d}".format(my_list[e]), end="")
             e += 1
         except (ValueError, TypeError):
             continue
-        except IndexError:
+        except (IndexError):
             raise
-    print("")
+    print()
     return e
