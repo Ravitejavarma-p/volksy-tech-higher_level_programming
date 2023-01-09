2a2e2448a5e7478e446b3a70b961734517d95cb5
Ravitejavarma-p <ravitejav@brightcomgroup.com>
2023-01-09 01:57:41 -0800

    0-Task

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,8 +3,9 @@ def safe_print_list(my_list=[], x=0):
     e = 0
     for i in range(0, x):
         try:
-            print("{}".format(my_list[i], end=""))
+            print("{}".format(my_list[e], end=""))
             e += 1
         except:
             pass
+    print('')    
     return e
