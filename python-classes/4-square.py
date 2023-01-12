9060cadd2edea4ceb8bad445aed61ee97cd7ed13
Ravitejavarma-p <ravitejav@brightcomgroup.com>
2023-01-11 20:12:29 -0800

    4-Task

---
+++ python-classes/4-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""creating class """
+
+
+class Square:
+    """Square with private instance attribute and instantiation"""
+    def _init_(self, size=0):
+        """initialize data"""
+        self.__size = size
+
+    def area(self):
+        """return square area"""
+        return(self.__size ** 2)
+
+    @property
+    def size(self):
+        """retrive size"""
+        return(self.__size)
+
+    @size.setter
+    def size(self, value):
+        """handling errors"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
