7e7d9ba626d39ffeabbe6b3ee870bf80805e6c57
Ravitejavarma-p <ravitejav@brightcomgroup.com>
2023-01-13 03:18:16 -0800

    5-Task

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -15,3 +15,14 @@ def text_indentation(text):
     c = 0
     while c < len(text) and text[c] == ' ':
         c += 1
+
+    while c < len(text):
+        print(text[c], end="")
+        if text[c] == "\n" or text[c] in ".?:":
+            if text[c] in ".?:":
+                print("\n")
+            c += 1
+            while c < len(text) and text[c] == ' ':
+                c += 1
+            continue
+        c += 1
