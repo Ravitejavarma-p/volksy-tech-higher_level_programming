a501e5d311c05e5c6d0fc5620a9bbd8cb532382a
Ravitejavarma-p <ravitejav@brightcomgroup.com>
2023-01-17 23:19:04 -0800

    1

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -6,5 +6,4 @@ def write_file(filename="", text=""):
     """writing a text"""
     if filename:
         with open(filename, mode='w', encoding="utf-8") as f:
-            for i in f:
-                print(i.write(text))
+            return(i.write(text))
