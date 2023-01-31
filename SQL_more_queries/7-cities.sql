cb64a0e43b0f2d90205eadc57e4f4f74064e9b43
Ravitejavarma-p <ravitejav@brightcomgroup.com>
2023-01-31 04:29:35 -0800

    7

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,4 +1,4 @@
 -- creating a table with primary key and foreign key
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities(id INT AUTO_INCREMENT NOT NULL, state_id INT NOT NULL, name VARCHAR(256) NOT NULL, PRIMARY KEY(id), FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.state(id);
+CREATE TABLE IF NOT EXISTS cities(id INT AUTO_INCREMENT NOT NULL, state_id INT NOT NULL, name VARCHAR(256) NOT NULL, PRIMARY KEY(id), FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.state(id));
