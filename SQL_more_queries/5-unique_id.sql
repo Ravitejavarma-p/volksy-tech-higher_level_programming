241af9b926ac95073bcb655c017b0abe414b3731
Ravitejavarma-p <ravitejav@brightcomgroup.com>
2023-01-31 03:54:03 -0800

    5

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,2 +1,2 @@
 -- creating table int with default value 1 and must be unique
-CREATE TABLE IF NOT EXISTS unique_id(id INT(1) UNIQUE, name VARCHAR(256)); 
+CREATE TABLE IF NOT EXISTS unique_id(id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
