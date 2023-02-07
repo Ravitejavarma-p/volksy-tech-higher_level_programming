438619b7291f4e8b5081c4d432e241a6505af791
Ravitejavarma-p <ravitejav@brightcomgroup.com>
2023-02-07 06:53:50 -0800

    5

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Asc order using joins"""
+
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM cities\
+               INNER JOIN states \
+               ON cities.state_id = states.id\
+               ORDER BY cities.id")
+    for i in c:
+        if i[4] == sys.argv[4]
+        print(i)
