15ece5b65c0a8f48c567309bc774a079e31dd216
Ravitejavarma-p <ravitejav@brightcomgroup.com>
2023-02-07 06:44:54 -0800

    4

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Asc order"""
+"""Asc order using joins"""
 
 
 import sys
@@ -8,6 +8,9 @@ import MySQLdb
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
-    c.execute("SELECT * FROM states ORDER BY cities.id")
+    c.execute("SELECT cities.id, cities.name, states.name \
+               FROM cities \
+               JOIN states \
+               ON cities.state_id = states.id")
     for i in c:
         print(i)
