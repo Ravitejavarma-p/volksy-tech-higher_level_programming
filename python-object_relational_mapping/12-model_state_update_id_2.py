939470314f08f47b42a323a5643b6a1504248ab9
Ravitejavarma-p <ravitejav@brightcomgroup.com>
2023-02-12 02:07:09 -0800

    orm

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,26 +1,23 @@
 #!/usr/bin/python3
-"""Updates into State obj from db"""
-import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
+"""
+Script to list all states in the states table.
+"""
+from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
+from sqlalchemy import (create_engine)
+import sys
 
 
-def update_to_state_obj():
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
-    session = Session(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-    to_change = session.query(State).filter(State.id == 2).first()
+    state = session.query(State).filter(State.id == 2)[0]
+    state.name = "New Mexico"
 
-    to_change.name = 'New Mexico'
     session.commit()
-
-    session.close()
-
-
-if __name__ == "__main__":
-    update_to_state_obj()
